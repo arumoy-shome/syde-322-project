c54b75d50d065332ce38a06ccbaffdc8b6ea9b0a
Arumoy Shome <arumoy.shome@shopify.com>
2016-02-09 23:18:04 -0500

    update README
    add basic landing page
    
    add sign in/sign up options to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  root to: 'homes#show', via: :get
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
