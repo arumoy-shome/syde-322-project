fc8c3ee9e5ecf573a199ef6771ecd08f846662b4
Arumoy Shome <arumoy.shome@gmail.com>
2016-02-18 23:43:56 -0500

    remove pending tests

---
+++ spec/features/user_visits_signup_page.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+feature "User visits sign up page" do
+  scenario "sucessfully" do
+    visit new_user_path
+
+    expect(page).to have_css("h1", text: "Sign up")
+  end
+
+  scenario "signs up sucessfully" do
+    user = build(:user, name: "Another Dummy",
+                email: "another@email.com", password_digest: "secret00")
+
+    visit new_user_path
+    fill_in("Name", with: user.name)
+    fill_in("Email", with: user.email)
+    fill_in("Password", with: user.password_digest)
+    click_button "Sign up"
+
+    expect(page).to have_css("p", text: "Signed in as: Another Dummy")
+  end
+end
