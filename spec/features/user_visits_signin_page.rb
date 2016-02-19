89878f805499049b21e16d4d93e8055ccba1e9de
Arumoy Shome <arumoy.shome@gmail.com>
2016-02-18 22:40:25 -0500

    address comments

--- spec/features/user_visits_signin_page.rb
+++ spec/features/user_visits_signin_page.rb
@@ -1,20 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
 feature "User visits sign in page" do
   scenario "sucessfully" do
     visit new_session_path
 
-    expect(page).to have_css('h1', text: 'Sign in')
+    expect(page).to have_css("h1", text: "Sign in")
   end
 
   scenario "signs in successfully" do
     user = build(:user)
 
     visit new_session_path
-    fill_in('Email', with: user.email)
-    fill_in('Password', with: user.password_digest)
-    click_button 'Sign in'
+    fill_in("Email", with: user.email)
+    fill_in("Password", with: user.password_digest)
+    click_button "Sign in"
 
-    expect(page).to have_css('p', text: 'Signed in as: Dummy User')
+    expect(page).to have_css("p", text: "Signed in as: Dummy User")
   end
 end
