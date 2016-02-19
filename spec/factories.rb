d616b0514c3408ef25fde34d2903a478ae122acc
Arumoy Shome <arumoy.shome@gmail.com>
2016-02-18 22:35:18 -0500

    fixes #34

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,1 +1,7 @@
-# define factories here
+FactoryGirl.define do
+  factory :user do
+    name "Dummy User"
+    email "dummy@email.com"
+    password_digest "secret00"
+  end
+end
--- spec/features/user_visits_signin_page.rb
+++ spec/features/user_visits_signin_page.rb
@@ -8,11 +8,13 @@ feature "User visits sign in page" do
   end
 
   scenario "signs in successfully" do
+    user = build(:user)
+
     visit new_session_path
-    fill_in('Email', with: 'arumoy.shome@gmail.com')
-    fill_in('Password', with: 'secret00')
+    fill_in('Email', with: user.email)
+    fill_in('Password', with: user.password_digest)
     click_button 'Sign in'
 
-    expect(page).to have_css('a', text: 'Sign out')
+    expect(page).to have_css('p', text: 'Signed in as: Dummy User')
   end
 end
