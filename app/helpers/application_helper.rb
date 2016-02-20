e51bdbd9245bdcf8d481ad305fe4bb6feb496412
Arumoy Shome <arumoy.shome@gmail.com>
2016-02-20 00:48:26 -0500

    fix placeholder image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,8 @@ module ApplicationHelper
     if user.image_url.present?
       user.image_url
     else
-      default_url = "users/default_image.png"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?" \
-      "s=48&d=#{CGI.escape(default_url)}"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
     end
   end
 end
