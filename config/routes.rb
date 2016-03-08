975f5f81a4d972b738f578ef60cc1503e9e8c66e
Arumoy Shome <arumoy.shome.work@gmail.com>
2016-03-08 12:18:18 -0500

    successfull facebook auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'homes#index', via: :get
   resources :users, only: [:new, :show, :create]
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get]
+  get '/auth/:provider/callback', to: 'sessions#create'
 end
