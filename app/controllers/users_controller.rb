be4214bf52468ce4b18d4cb2deb9897f351ea192
Arumoy Shome <arumoy.shome.work@gmail.com>
2016-03-08 13:00:33 -0500

    remove redundant code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,9 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: [:new, :create]
-
   def new
     @user = User.new
   end
 
   def show
-  end
-
-  def create
-    @user = sign_up(user_params)
-
-    if @user.valid?
-      sign_in(@user)
-      redirect_to root_path
-    else
-      render :new
-    end
   end
 
   private
