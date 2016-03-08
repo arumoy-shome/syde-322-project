5705e719f7346f30625285972622d908b596a849
Arumoy Shome <arumoy.shome.work@gmail.com>
2016-03-08 15:46:52 -0500

    drop password_digest column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308171820) do
+ActiveRecord::Schema.define(version: 20160308203921) do
 
   create_table "authorizations", force: :cascade do |t|
     t.string   "provider"
@@ -50,10 +50,9 @@ ActiveRecord::Schema.define(version: 20160308171820) do
   add_index "tutors", ["user_id"], name: "index_tutors_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "email",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "name"
     t.string   "image_url"
   end
