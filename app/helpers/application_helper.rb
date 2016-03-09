e872a62446d90870f9f37ed5bb9cc98392a86797
sujan-sube <ssujan1@hotmail.com>
2016-03-08 19:22:00 -0500

    added default profile avatar images.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,7 @@ module ApplicationHelper
     if user.image_url.present?
       user.image_url
     else
-      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    	"default.png"
     end
   end
 end
