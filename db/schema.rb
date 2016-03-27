55f861405c732f69a22f9f80814934141fcb85a7
matraj <maathusan@gmail.com>
2016-03-27 15:28:25 -0400

    add is tutor column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327173255) do
+ActiveRecord::Schema.define(version: 20160327161040) do
 
   create_table "authorizations", force: :cascade do |t|
     t.string   "provider"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20160327173255) do
     t.datetime "updated_at", null: false
     t.string   "name"
     t.string   "image_url"
+    t.boolean  "is_tutor"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
