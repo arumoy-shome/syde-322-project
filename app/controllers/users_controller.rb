a6c9f3e8d1add78368e7d506801aa2b718096512
matraj <maathusan@gmail.com>
2016-03-28 16:17:04 -0400

    Added setup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,20 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if signed_in? && current_user.is_tutor.nil?
+      redirect_to setup_path
+    end
+  end
+
+  def update
+    current_user.is_tutor = true
+    flash[:notice] = "Successfully is tutored"
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:is_tutor)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: "homes#index", via: :get
-  resources :users, only: [:show]
+  resources :users, only: [:show, :update]
   resource :calendar, only: [:show]
   resource :session, only: [:destroy]
   get "/auth/:provider/callback", to: "sessions#create"
   get "/tutors", to: "users#tutor"
+  get "/setup", to: "homes#setup"
 end
